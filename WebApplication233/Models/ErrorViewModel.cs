34ffd8f880a9e947d1431ff1c364392d4567788e
HARISH G <137024093+GHARISH007@users.noreply.github.com>
2025-06-07 16:26:33 +0530

    last commit

---
+++ WebApplication233/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication233.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
